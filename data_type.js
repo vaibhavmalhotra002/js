8a54e8041a44af6555de178f3358f8a659f09f5e
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-21 17:21:25 +0530

    first commit

---
+++ data_type.js
@@ -0,0 +1,21 @@
+/*
+There are 7 types of datatype in js (nnbbsu)
+null
+number
+boolean
+Bigint
+string
+symbol
+undefined
+*/
+let a= null;
+let b=789;
+let c=true;
+let d=BigInt("783") +BigInt("7")
+let e="vaibhav"
+let f= Symbol ("A symbol")
+let g=undefined 
+let h
+
+console.log(a,b,c,d,e,f,g,h)
+console.log(typeof d)
