fc2ffbb0babc3588f840bb15abf80af639f0070a
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-21 17:31:32 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,12 @@
+//objects are key value pairs in js which we can lookup later
+const item ={
+    "vaibhav": true,
+    "dev"    : false,
+    "Aditya ": 69,
+    "varun"  : undefined 
+
+}
+console.log(item["vaibhav"])
+console.log(item["dev"])
+console.log(item["Aditya "])
+console.log(item["varun"])
