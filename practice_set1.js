7d98acb524ade3ffe06b2499bb8d58ebebe8790c
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-21 18:09:47 +0530

    first commit

--- practice_set1.js
+++ practice_set1.js
@@ -5,3 +5,11 @@ console.log(a+b) //concatination takes place here
 
 //use typeof operator to find the datatype of the string in above ques.
 console.log(typeof (a+b))
+
+//create a const object in js &can you make it hold a number later?
+/*
+const a ={
+    name="vaibhav"
+    class=10
+}
+a= 45; //Will give a error */
