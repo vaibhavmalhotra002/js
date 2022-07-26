2d53058d3c11d42751dd8074c8339a426b0c8875
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-26 20:11:06 +0530

    commit

---
+++ Boot_dev.js
@@ -0,0 +1,19 @@
+let elonHeight = 5
+let saraHeight = 10
+let jillHeight = 7
+let bobHeight = 6
+
+// don't edit above this line
+
+let bobTallerThanElon =
+let saraTallerThanElon =
+let jillTallerThanSara =
+
+// don't edit below this line
+
+console.log(typeof bobTallerThanElon)
+console.log(bobTallerThanElon)
+console.log(typeof saraTallerThanElon)
+console.log(saraTallerThanElon)
+console.log(typeof jillTallerThanSara)
+console.log(jillTallerThanSara)
---
+++ operators.js
@@ -0,0 +1,10 @@
+// 77;
+// "vaibhav";
+
+console.log("Operators In Js");
+let a=38;
+let b=3;
+console.log("a+b =" , a+b);
+console.log("a-b =" , a-b);
+console.log("a*b =" , a*b);
+console.log("a/b =" , a/b);
