fb8d223f9ac5ccb46287510c35fffd4d19abee8c
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-27 17:02:08 +0530

    c

--- operators.js
+++ operators.js
@@ -2,9 +2,20 @@
 // "vaibhav";
 
 console.log("Operators In Js");
-let a=38;
+let a=10;
 let b=3;
+//Arithmetic operators
 console.log("a+b =" , a+b);
 console.log("a-b =" , a-b);
 console.log("a*b =" , a*b);
 console.log("a/b =" , a/b);
+console.log("a**b =" , a**b); //38 to the power 3
+console.log("a%b =", a%b)
+console.log("a++ =", a++)
+console.log("++a =", ++a)
+console.log("a-- =", a--)
+console.log("--a =", --a) 
+
+//Assignment operators
+a+=5 //same as a= a+5
+console.log (a);
