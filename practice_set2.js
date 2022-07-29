ef65255cbd5fc9458d809f4d00045cff2be64e3c
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-29 11:18:54 +0530

    practice

---
+++ practice_set2.js
@@ -0,0 +1,35 @@
+//use logical operators to find whether a person's age lies between 10 & 20?
+/*let age = prompt("What is your age")
+if (age>10 && age<20){
+    console.log("Your age lies between 10 & 20 ")
+}
+else{
+    console.log("Your age doesn't lies between 10 & 20 ")
+}*/
+//use switch
+// let age = prompt("What is your age")
+// switch(age){
+//     case '12':
+//         console.log("You are not a teen")
+//         break;
+//         case '13':
+//         console.log("You entered teenage")
+//         break;
+//         default:
+//         console.log("You are no one")
+// }
+// //number is div by both 2 &3
+// let num =prompt("Enter Your Number:")
+// num=Number.parseInt(num)
+// if(num % 2==0 && num%3==0){
+//     console.log("Your number is div by 2 & 3  ")
+// }
+// else{
+//     console.log("Your number is not div by 2&3")
+// }
+
+//ternary operator
+let age =12;
+let a= age>18?"You can drive":"You cannot drive";
+console.log(a)
+
