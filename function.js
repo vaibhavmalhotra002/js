f1e8802b6f22d49e1d0a152e8c78152e160be77e
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-08-04 14:54:17 +0530

    done

---
+++ function.js
@@ -0,0 +1,22 @@
+const hello= ()=>{
+    console.log("Hey vaibhav how are you")
+
+}
+
+function oneplusavg(x,y){
+    return 1 + (x+y) /2
+}
+
+const sum= (p,q)=>{
+     return p+q
+}
+
+let a = 1;
+let b = 2;
+let c = 4;
+
+hello();
+console.log("One plus avg is" , oneplusavg(a,b))
+console.log("One plus avg is" , oneplusavg(b,c))
+console.log("One plus avg is" , oneplusavg(c,a))
+console.log(sum(0,9))
