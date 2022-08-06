fdc63b159313585b97a2bdc4eff9db76e22ade7d
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-08-06 09:35:13 +0530

    hyryder

---
+++ strings.js
@@ -0,0 +1,9 @@
+//template literals
+let boy1 ='kunal'
+let boy2 ='vaibhav'
+
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+//escape sequence character
+let fruit='Banana\'s'
