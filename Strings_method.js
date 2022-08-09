5426c8b8a5cf1ea132e9f09b2dda224e73cd48b2
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-08-09 23:31:43 +0530

    done

--- Strings_method.js
+++ Strings_method.js
@@ -1,11 +1,15 @@
-let name="Vaibhav"
+// let name="Vaibhav"
 // console.log(name.length)
 // console.log(name.toUpperCase())
 // console.log(name.toLowerCase())
 // console.log(name.slice(2,5))
 // console.log(name.slice(2))
 // console.log(name.replace("v", "vi"))
-let friend="Kunal";
-console.log(name.concat(" is a friend of " , friend))
-let friend_2="     Neena      "
-console.log(friend_2.trim())
+// let friend="Kunal";
+// console.log(name.concat(" is a friend of " , friend))
+// let friend_2="     Neena      "
+// console.log(friend_2.trim())
+let dost ="Vaibhav" + "Kunal"
+for (let i = 0; i < dost.length; i++) {
+    console.log(dost[i]);
+    }
