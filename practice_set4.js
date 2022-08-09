baf3e797cfab0542a4c3c7c4a4d620680a1670a6
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-08-09 23:59:32 +0530

    strings complete

---
+++ practice_set4.js
@@ -0,0 +1,14 @@
+//Q1 what will this print
+console.log("har\"".length)
+//q2 
+let text = "Hello world, welcome to the universe.";
+let result = text.includes("world");
+console.log(result)
+//Q3
+let str="Vaibhav IS A Great guy"
+console.log(str.toUpperCase())
+//Q4
+let sen="Please give Rs 2000";
+let amount=(Number.parseInt(sen.slice(15)))
+console.log(amount)
+console.log(typeof amount)
