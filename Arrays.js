84cc632774dd89781fa1be1492d8abe956f409d3
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-08-10 00:17:52 +0530

    arrays

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+let marks =[12,13,14,15,"Not Present",null]
+console.log(marks)
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[4])
+console.log(marks[5])
+console.log("The Length of marks is" ,marks.length)
+marks[6]=78;//adding value to array
+console.log(marks[6])
+marks[0]=98;//changing value in array
+console.log(marks[0])
+
+
